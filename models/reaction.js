7ef1ce37e4a428bf7c60ed4c3bd4c7b6f1a64d2c
mvill <119754846+migavg@users.noreply.github.com>
2023-05-28 17:05:03 -0700

    created reaction model schema

---
+++ models/reaction.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            //use mongoose object id data and set to default
+        },
+
+        reactionBody: {
+            type: String,
+            required: true,
+            max_length: 280,
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            //insert date getter and set to default
+        },
+    }
+    );
+
+    const reaction = model('reactio', reactionSchema);
+
+    module.exports = reaction;
